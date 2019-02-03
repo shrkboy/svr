37986659f83faeb1f5f539d30d18c30f72040914
shrkboy <ilhamn33@gmail.com>
2019-02-03 19:58:33 +0800

    minor update

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -21,8 +21,8 @@
     <div class="container-fluid">
         <div class="col-md-auto">
             <h4>Display</h4>
-            <form class="p-3">
-                <div class="form-group" style="width: 30%">
+            <form>
+                <div class="form-group">
                     <label for="branch">Select branch</label>
                     <select name="branch" id="branch" class="form-control">
                         <option hidden selected value="null">Select...</option>
@@ -31,9 +31,7 @@
                         <option value="">Branch #3</option>
                     </select>
                 </div>
-
-                <table class="table">
-
+                <table class="table table-striped" style="table-layout: fixed">
                     <thead>
                     <tr>
                         <th scope="col">Models</th>
@@ -43,31 +41,27 @@
                         <th scope="col">Streamer</th>
                     </tr>
                     </thead>
-
                     <tbody>
                     <tr>
-                        <th scope="row">Model name</th>
+                        <td scope="row">Model name</td>
                         <td>
-                            <input type="number" min=0 max=999 name="display-qty" class="form-control">
+                            <input type="number" min=0 max=999 name="display-qty" class="form-control" style="width: 100%">
                         </td>
                         <td>
-                            <input type="number" min=0 max=999 name="talker" class="form-control">
+                            <input type="number" min=0 max=999 name="talker" class="form-control" style="width: 100%">
                         </td>
                         <td>
-                            <input type="number" min=0 max=999 name="flyer" class="form-control">
+                            <input type="number" min=0 max=999 name="flyer" class="form-control" style="width: 100%">
                         </td>
                         <td>
-                            <input type="number" min=0 max=999 name="streamer" class="form-control">
+                            <input type="number" min=0 max=999 name="streamer" class="form-control" style="width: 100%">
                         </td>
                     </tr>
                     </tbody>
-
                 </table>
-
                 <div class="text-right">
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
-
             </form>
         </div>
     </div>
