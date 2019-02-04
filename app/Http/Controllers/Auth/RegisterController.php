3edc9c2a9847931af88e57c24e56f93ccb97f466
shrkboy <ilhamn33@gmail.com>
2019-02-04 21:58:24 +0800

    Give auth to register route

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/display';
 
     /**
      * Create a new controller instance.
@@ -37,7 +37,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('is_admin');
     }
 
     /**
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            ‘is_admin’ => $data[‘is_admin’],
             'password' => Hash::make($data['password']),
         ]);
     }
