bc8b7dcbeaf39bd0b2217e9e433d2622185a8727
shrkboy <ilhamn33@gmail.com>
2019-02-04 18:47:38 +0800

    Minor Update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Branch;
 use App\BikeModel;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -48,7 +49,7 @@ class ReportController extends Controller
     public function store(Request $request)
     {
         //
-        return $request->all();
+        return User::all();
     }
 
     /**
