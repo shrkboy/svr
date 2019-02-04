ab1e77d4cfd7d9629ea3ea8a49919cfc4e34aa07
shrkboy <ilhamn33@gmail.com>
2019-02-04 13:08:50 +0800

    Minor Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    {{--<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>--}}
     @yield('head-script')
-    {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
