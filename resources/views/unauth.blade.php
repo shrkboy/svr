5dea35cf754639ad090156f5ffaa7d81ae63c803
shrkboy <ilhamn33@gmail.com>
2019-02-04 22:43:07 +0800

    Change Unauth View

--- resources/views/unauth.blade.php
+++ resources/views/unauth.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
+
+@section('navmenu')
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="{{url('/display')}}">Home</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="{{url('/display')}}">User</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{url('/display')}}">Retail</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{url('/display')}}">Calendar</a>
+        </li>
+    </ul>
+@stop
+
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
