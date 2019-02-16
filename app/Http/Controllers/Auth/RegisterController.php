eed4409f2bdd14d2031c0c1770cf383b4a42f24c
shrkboy <ilhamn33@gmail.com>
2019-02-16 21:53:30 +0800

    Fixed insert user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/users';
 
     /**
      * Create a new controller instance.
